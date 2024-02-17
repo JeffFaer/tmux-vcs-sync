e6853cf118c2a5c03b9beb88df1bcb7f4437d00d
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-02-16 17:59:27 -0700

    Set user.name and user.email.

--- git/git/git_test.go
+++ git/git/git_test.go
@@ -12,11 +12,11 @@ import (
 func TestRepoAPI(t *testing.T) {
 	newGitRepo := func(name string) (api.Repository, error) {
 		dir := t.TempDir()
-		if err := git.Command("-C", dir, "init", name).Run(); err != nil {
+		if err := git.Command("-C", dir, "-c", "user.name=test", "-c", "user.email=test@example.com", "init", name).Run(); err != nil {
 			return nil, fmt.Errorf("git init: %w", err)
 		}
 		dir = filepath.Join(dir, name)
-		if err := git.Command("-C", dir, "commit", "--allow-empty", "--message", "Initial commit.").Run(); err != nil {
+		if err := git.Command("-C", dir, "-c", "user.name=test", "-c", "user.email=test@example.com", "commit", "--allow-empty", "--message", "Initial commit.").Run(); err != nil {
 			return nil, fmt.Errorf("git initial commit: %v", err)
 		}
 		return git.Repository(dir)
