dd2e1dcbc27a2280bf46938f3437101331cc59b6
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-02-18 10:40:53 -0700

    test(git): Make sure none of the git commands are using gitconfigs.

--- git/git/git_test.go
+++ git/git/git_test.go
@@ -10,8 +10,18 @@ import (
 	"github.com/JeffFaer/tmux-vcs-sync/api/repotest"
 )
 
+var testGit git
+
+func init() {
+	exec, err := exec.Lookup("git")
+	if err != nil {
+		panic(err)
+	}
+	testGit = git{testGitCmd{exec}}
+}
+
 type testGitCmd struct {
-	git gitCmd
+	git commander
 }
 
 func (git testGitCmd) Command(args ...string) *exec.Command {
@@ -22,17 +32,16 @@ func (git testGitCmd) Command(args ...string) *exec.Command {
 }
 
 func TestRepoAPI(t *testing.T) {
-	git := testGitCmd{git}
 	newGitRepo := func(name string) (api.Repository, error) {
 		dir := t.TempDir()
-		if err := git.Command("-C", dir, "init", name).Run(); err != nil {
+		if err := testGit.Command("-C", dir, "init", name).Run(); err != nil {
 			return nil, fmt.Errorf("git init: %w", err)
 		}
 		dir = filepath.Join(dir, name)
-		if err := git.Command("-C", dir, "commit", "--allow-empty", "--message", "Initial commit.").Run(); err != nil {
+		if err := testGit.Command("-C", dir, "commit", "--allow-empty", "--message", "Initial commit.").Run(); err != nil {
 			return nil, fmt.Errorf("git initial commit: %v", err)
 		}
-		return git.git.Repository(dir)
+		return testGit.Repository(dir)
 	}
 	repotest.RepoTests(t, newGitRepo, repotest.Options{NoopRenameIsOK: true})
 }
