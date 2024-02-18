5882775b6ca83df4ec7a8f0b48b1613a4d132ad9
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-02-18 15:24:06 -0700

    feat(cli): Optionally generate shell completion files. (#9)

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -6,15 +6,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
 
-var Default = Build
+var Default = Build.Build
 
-func Build() error {
+var Aliases = map[string]interface{}{
+	"build":   Build.Build,
+	"install": Install.Install,
+}
+
+type Build mg.Namespace
+type Install mg.Namespace
+
+func (Build) Build() error {
 	fmt.Println("Building...")
 	return sh.Run("go", "build", "-o=tmux-vcs-sync", ".")
+}
+
+func (Build) Completion(shell string) error {
+	mg.Deps(Build.Build)
+	fmt.Printf("Generating %s completion...\n", shell)
+	return os.WriteFile(fmt.Sprintf("tvs_completion.%s", shell), []byte(fmt.Sprintf("source <(tmux-vcs-sync completion %q)", shell)), 0644)
 }
 
 func Test() error {
@@ -22,9 +38,46 @@ func Test() error {
 	return sh.Run("go", "test", "./...")
 }
 
-func Install() error {
+func (Install) Install() error {
 	fmt.Println("Installing...")
 	return sh.Run("go", "install", ".")
+}
+
+func (Install) Completion(shell string) error {
+	mg.Deps(mg.F(Build.Completion, shell))
+	dir, err := determineCompletionDir(shell)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Installing %s completion to %s...\n", shell, dir)
+	name := fmt.Sprintf("tvs_completion.%s", shell)
+	return sh.Run("cp", name, filepath.Join(dir, name))
+}
+
+func determineCompletionDir(shell string) (string, error) {
+	if shell != "bash" {
+		return "", fmt.Errorf("the author only uses bash so they don't know where other shell completion files are supposed to go")
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	for _, dir := range []string{
+		filepath.Join(home, "bash_completion.d"),
+		filepath.Join(home, "bashrc.d/bash_completion.d"),
+		"/etc/bash_completion.d",
+	} {
+		if stat, err := os.Stat(dir); err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+			return "", err
+		} else if stat.IsDir() {
+			return dir, nil
+		}
+	}
+
+	return "", fmt.Errorf("did not find any appropriate directory to install %s completions", shell)
 }
 
 func Clean() {
