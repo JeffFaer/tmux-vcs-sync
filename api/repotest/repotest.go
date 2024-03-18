2d8ddf4e53d08cff8c07e11a9ed0c3e8a9fd4b86
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-03-17 18:12:07 -0600

    feat(api): Add a new Sort method so that repositories can choose their own ordering.

--- api/repotest/repotest.go
+++ api/repotest/repotest.go
@@ -31,6 +31,7 @@ func (vcs fakeVCS) Repository(dir string) (api.Repository, error) {
 		dir:       dir,
 		cur:       "root",
 		workUnits: map[string]string{"root": ""},
+		children:  map[string]map[string]bool{"root": make(map[string]bool)},
 	}, nil
 }
 
@@ -40,6 +41,7 @@ type fakeRepo struct {
 
 	cur       string
 	workUnits map[string]string
+	children  map[string]map[string]bool
 }
 
 func (repo *fakeRepo) VCS() api.VersionControlSystem {
@@ -68,6 +70,44 @@ func (repo *fakeRepo) List(prefix string) ([]string, error) {
 	return ret, nil
 }
 
+func (repo *fakeRepo) Sort(workUnits []string) error {
+	allowed := make(map[string]bool)
+	for _, wu := range workUnits {
+		if _, ok := repo.workUnits[wu]; !ok {
+			return fmt.Errorf("work unit %q does not exist", wu)
+		}
+		allowed[wu] = true
+	}
+
+	var topo []string
+	s := map[string]bool{"root": true}
+	for len(s) > 0 {
+		var n string
+		for m := range s {
+			if n == "" {
+				n = m
+			} else {
+				n = min(n, m)
+			}
+		}
+		delete(s, n)
+		topo = append(topo, n)
+		for child := range repo.children[n] {
+			s[child] = true
+		}
+	}
+
+	var i int
+	for _, n := range topo {
+		if allowed[n] {
+			workUnits[i] = n
+			i++
+		}
+	}
+
+	return nil
+}
+
 func (repo *fakeRepo) New(workUnitName string) error {
 	return repo.commit(workUnitName, "root")
 }
@@ -85,6 +125,8 @@ func (repo *fakeRepo) commit(workUnitName, parent string) error {
 		return fmt.Errorf("parent work unit %q does not exist", parent)
 	}
 	repo.workUnits[workUnitName] = parent
+	repo.children[workUnitName] = make(map[string]bool)
+	repo.children[parent][workUnitName] = true
 	repo.cur = workUnitName
 	return nil
 }
@@ -94,8 +136,13 @@ func (repo *fakeRepo) Rename(workUnitName string) error {
 		return fmt.Errorf("work unit %q already exists", workUnitName)
 	}
 	parent := repo.workUnits[repo.cur]
+	children := repo.children[repo.cur]
 	delete(repo.workUnits, repo.cur)
+	delete(repo.children, repo.cur)
+	delete(repo.children[parent], repo.cur)
 	repo.workUnits[workUnitName] = parent
+	repo.children[workUnitName] = children
+	repo.children[parent][workUnitName] = true
 	repo.cur = workUnitName
 	return nil
 }
