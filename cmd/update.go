JeffFaer/tmux-vcs-sync

commit 2c3a09a63cb5589a5cfbdc9dc8dad7cb54ce9da5

feat(display-menu): Add a new (hidden) command "display-menu".
This command invokes the tmux display-menu command to let you switch
your client to another session, but orders things usefully.

  - state: Tracks unknown sessions.
  - state: Indexes sessions by ID so that we can return api.Repositories
    for them.
  - state: Exposes its logic on how to turn WorkUnitNames into tmux
    session names.

cmd/update.go
@@ -44,7 +44,7 @@ var updateCommand = &cobra.Command{
 func suggestWorkUnitNames(toComplete string) []string {
 	var repos map[state.RepoName]api.Repository
 	if srv := tmux.MaybeCurrentServer(); srv != nil {
-		st, err := state.New(srv)
+		st, err := state.New(srv, api.Registered)
 		if err != nil {
 			slog.Warn("Could not determine repositories from tmux server.", "server", srv, "error", err)
 		} else {
@@ -84,7 +84,7 @@ func update() error {
 	if sesh := tmux.MaybeCurrentSession(); sesh == nil {
 		// Executed outside of tmux. Attach to the proper tmux session.
 		srv, _ := tmux.CurrentServerOrDefault()
-		state, err := state.New(srv)
+		state, err := state.New(srv, api.Registered)
 		if err != nil {
 			return err
 		}
@@ -122,7 +122,7 @@ func updateTmux(srv tmux.Server, st *state.State, repo api.Repository, workUnit 
 
 func updateTo(workUnitName string) error {
 	srv, hasCurrentServer := tmux.CurrentServerOrDefault()
-	st, err := state.New(srv)
+	st, err := state.New(srv, api.Registered)
 	if err != nil {
 		return err
 	}

cmd/cleanup.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/JeffFaer/tmux-vcs-sync/api"
 	"github.com/JeffFaer/tmux-vcs-sync/tmux"
 	"github.com/JeffFaer/tmux-vcs-sync/tmux/state"
 	"github.com/spf13/cobra"
@@ -21,7 +22,7 @@ var cleanupCommand = &cobra.Command{
 
 func cleanup() error {
 	srv, _ := tmux.CurrentServerOrDefault()
-	st, err := state.New(srv)
+	st, err := state.New(srv, api.Registered)
 	if err != nil {
 		return err
 	}

cmd/display_menu.go
@@ -1,0 +1,134 @@
+package cmd
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/JeffFaer/go-stdlib-ext/morecmp"
+	"github.com/JeffFaer/go-stdlib-ext/moremaps"
+	"github.com/JeffFaer/tmux-vcs-sync/api"
+	"github.com/JeffFaer/tmux-vcs-sync/tmux"
+	"github.com/JeffFaer/tmux-vcs-sync/tmux/state"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/maps"
+)
+
+func init() {
+	rootCmd.AddCommand(displayMenuCommand)
+}
+
+var displayMenuCommand = &cobra.Command{
+	Use:    "display-menu",
+	Hidden: true,
+	Short:  "Run tmux display-menu to switch to a new session.",
+	Args:   cobra.ExactArgs(0),
+	RunE: func(*cobra.Command, []string) error {
+		return displayMenu()
+	},
+}
+
+func displayMenu() error {
+	curSesh, err := tmux.CurrentSession()
+	if err != nil {
+		return err
+	}
+	curClient, err := tmux.CurrentClient()
+	if err != nil {
+		// This shouldn't be possible since we have a current session.
+		return err
+	}
+
+	menu, err := createMenu(curSesh, api.Registered)
+	if err != nil {
+		return err
+	}
+	return curClient.DisplayMenu(menu)
+}
+
+func createMenu(curSesh tmux.Session, vcs api.VersionControlSystems) ([]tmux.MenuElement, error) {
+	st, err := state.New(curSesh.Server(), vcs)
+	if err != nil {
+		return nil, err
+	}
+
+	sessions := st.Sessions()
+	sessionsByRepo := make(map[state.RepoName]map[string]tmux.Session)
+	for n, sesh := range sessions {
+		if sessionsByRepo[n.RepoName] == nil {
+			sessionsByRepo[n.RepoName] = make(map[string]tmux.Session)
+		}
+		sessionsByRepo[n.RepoName][n.WorkUnit] = sesh
+	}
+
+	repoCmp := morecmp.Comparing(func(n state.RepoName) string { return n.VCS }).
+		AndThen(morecmp.Comparing(func(n state.RepoName) string { return n.Repo }))
+	repoNames := moremaps.SortedKeysFunc(sessionsByRepo, repoCmp)
+
+	type session struct {
+		name string
+		id   string
+	}
+	var orderedSessions [][]session
+	repos := st.Repositories()
+	for _, n := range repoNames {
+		repo := repos[n]
+		sessions := sessionsByRepo[n]
+		workUnits := maps.Keys(sessions)
+		if err := repo.Sort(workUnits); err != nil {
+			return nil, err
+		}
+		var group []session
+		for _, wu := range workUnits {
+			sesh := sessions[wu]
+			n := state.NewWorkUnitName(repo, wu)
+			group = append(group, session{st.SessionName(n), sesh.ID()})
+		}
+		orderedSessions = append(orderedSessions, group)
+	}
+
+	unknownSessions := st.UnknownSessions()
+	if len(unknownSessions) > 0 {
+		var group []session
+		for _, n := range moremaps.SortedKeys(unknownSessions) {
+			group = append(group, session{n, unknownSessions[n].ID()})
+		}
+		orderedSessions = append(orderedSessions, group)
+	}
+
+	hasCurrentSession := func(group []session) bool {
+		for _, s := range group {
+			if s.id == curSesh.ID() {
+				return true
+			}
+		}
+		return false
+	}
+	slices.SortStableFunc(orderedSessions, morecmp.ComparingFunc(hasCurrentSession, morecmp.TrueFirst()))
+
+	var menu []tmux.MenuElement
+	keys := strings.Split("0123456789wertyuiopasdfghjklzxcvbnm", "")
+	for i, group := range orderedSessions {
+		if i > 0 {
+			menu = append(menu, tmux.MenuSpacer{})
+		}
+		for _, sesh := range group {
+			name := sesh.name
+			key := keys[0]
+			keys = keys[1:]
+			if sesh.id == curSesh.ID() {
+				key = "q"
+				name = "*" + name
+			} else {
+				name = " " + name
+			}
+			menu = append(menu, tmux.MenuEntry{
+				Name: name,
+				Key:  key,
+				// TODO: Should this be a `run-shell tmux-vcs-sync update` instead?
+				Command: fmt.Sprintf("switch-client -t %s", sesh.id),
+			})
+		}
+	}
+	return menu, nil
+}

cmd/display_menu_test.go
@@ -1,0 +1,180 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/JeffFaer/tmux-vcs-sync/api"
+	"github.com/JeffFaer/tmux-vcs-sync/api/repotest"
+	"github.com/JeffFaer/tmux-vcs-sync/tmux"
+	"github.com/JeffFaer/tmux-vcs-sync/tmux/tmuxtest"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func TestDisplayMenu(t *testing.T) {
+	for i, tc := range []struct {
+		name string
+
+		sessions []tmux.NewSessionOptions
+		current  tmux.NewSessionOptions
+		vcs      api.VersionControlSystems
+
+		want []tmux.MenuElement
+	}{
+		{
+			name: "SingleSession",
+
+			current: tmux.NewSessionOptions{Name: "foo", StartDir: "testing/repo"},
+			vcs: api.VersionControlSystems{
+				repotest.NewVCS("testing/", repotest.RepoConfig{
+					Name: "repo",
+					WorkUnits: map[string][]string{
+						repotest.DefaultWorkUnitName: {"foo"},
+					},
+				}),
+			},
+
+			want: []tmux.MenuElement{
+				tmux.MenuEntry{Name: "*foo", Key: "q"},
+			},
+		},
+		{
+			name: "SingleSession_Unknown",
+
+			current: tmux.NewSessionOptions{Name: "foo", StartDir: "someOtherDir"},
+			vcs: api.VersionControlSystems{
+				repotest.NewVCS("testing/"),
+			},
+
+			want: []tmux.MenuElement{
+				tmux.MenuEntry{Name: "*foo", Key: "q"},
+			},
+		},
+		{
+			name: "SingleRepo_TopologicallySorted",
+
+			sessions: []tmux.NewSessionOptions{
+				{Name: "z", StartDir: "testing/repo"},
+				{Name: "x", StartDir: "testing/repo"},
+				{Name: "y", StartDir: "testing/repo"},
+			},
+			current: tmux.NewSessionOptions{Name: repotest.DefaultWorkUnitName, StartDir: "testing/repo"},
+			vcs: api.VersionControlSystems{
+				repotest.NewVCS("testing/", repotest.RepoConfig{
+					Name: "repo",
+					WorkUnits: map[string][]string{
+						repotest.DefaultWorkUnitName: {"z"},
+						"z":                          {"y"},
+						"y":                          {"x"},
+					},
+				}),
+			},
+
+			want: []tmux.MenuElement{
+				tmux.MenuEntry{Name: "*" + repotest.DefaultWorkUnitName, Key: "q"},
+				tmux.MenuEntry{Name: " z", Key: "1"},
+				tmux.MenuEntry{Name: " y", Key: "2"},
+				tmux.MenuEntry{Name: " x", Key: "3"},
+			},
+		},
+		{
+			name: "MultipleRepos",
+
+			sessions: []tmux.NewSessionOptions{
+				{Name: "bar", StartDir: "testing/repo1"},
+				{Name: "baz", StartDir: "testing/repo1"},
+				{Name: "foo", StartDir: "testing/repo2"},
+				{Name: "bar", StartDir: "testing/repo2"},
+				{Name: "baz", StartDir: "testing/repo2"},
+				{Name: "foo", StartDir: "someOtherDir"},
+				{Name: "bar", StartDir: "someOtherDir"},
+				{Name: "baz", StartDir: "someOtherDir"},
+			},
+			current: tmux.NewSessionOptions{Name: "foo", StartDir: "testing/repo1"},
+			vcs: api.VersionControlSystems{
+				repotest.NewVCS("testing/", repotest.RepoConfig{
+					Name:      "repo1",
+					WorkUnits: map[string][]string{repotest.DefaultWorkUnitName: {"foo", "bar", "baz"}},
+				}, repotest.RepoConfig{
+					Name:      "repo2",
+					WorkUnits: map[string][]string{repotest.DefaultWorkUnitName: {"foo", "bar", "baz"}},
+				}),
+			},
+
+			want: []tmux.MenuElement{
+				tmux.MenuEntry{Name: " repo1>bar", Key: "0"},
+				tmux.MenuEntry{Name: " repo1>baz", Key: "1"},
+				tmux.MenuEntry{Name: "*repo1>foo", Key: "q"},
+				tmux.MenuSpacer{},
+				tmux.MenuEntry{Name: " repo2>bar", Key: "3"},
+				tmux.MenuEntry{Name: " repo2>baz", Key: "4"},
+				tmux.MenuEntry{Name: " repo2>foo", Key: "5"},
+				tmux.MenuSpacer{},
+				tmux.MenuEntry{Name: " bar", Key: "6"},
+				tmux.MenuEntry{Name: " baz", Key: "7"},
+				tmux.MenuEntry{Name: " foo", Key: "8"},
+			},
+		},
+		{
+			name: "MultipleRepos_CurrentIsUnknown",
+
+			sessions: []tmux.NewSessionOptions{
+				{Name: "foo", StartDir: "testing/repo1"},
+				{Name: "bar", StartDir: "testing/repo1"},
+				{Name: "baz", StartDir: "testing/repo1"},
+				{Name: "foo", StartDir: "testing/repo2"},
+				{Name: "bar", StartDir: "testing/repo2"},
+				{Name: "baz", StartDir: "testing/repo2"},
+				{Name: "bar", StartDir: "someOtherDir"},
+				{Name: "baz", StartDir: "someOtherDir"},
+			},
+			current: tmux.NewSessionOptions{Name: "foo", StartDir: "someOtherDir"},
+			vcs: api.VersionControlSystems{
+				repotest.NewVCS("testing/", repotest.RepoConfig{
+					Name:      "repo1",
+					WorkUnits: map[string][]string{repotest.DefaultWorkUnitName: {"foo", "bar", "baz"}},
+				}, repotest.RepoConfig{
+					Name:      "repo2",
+					WorkUnits: map[string][]string{repotest.DefaultWorkUnitName: {"foo", "bar", "baz"}},
+				}),
+			},
+
+			want: []tmux.MenuElement{
+				tmux.MenuEntry{Name: " bar", Key: "0"},
+				tmux.MenuEntry{Name: " baz", Key: "1"},
+				tmux.MenuEntry{Name: "*foo", Key: "q"},
+				tmux.MenuSpacer{},
+				tmux.MenuEntry{Name: " repo1>bar", Key: "3"},
+				tmux.MenuEntry{Name: " repo1>baz", Key: "4"},
+				tmux.MenuEntry{Name: " repo1>foo", Key: "5"},
+				tmux.MenuSpacer{},
+				tmux.MenuEntry{Name: " repo2>bar", Key: "6"},
+				tmux.MenuEntry{Name: " repo2>baz", Key: "7"},
+				tmux.MenuEntry{Name: " repo2>foo", Key: "8"},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			srv := tmuxtest.NewServer(i)
+			for _, sesh := range tc.sessions {
+				if _, err := srv.NewSession(sesh); err != nil {
+					t.Errorf("tmux.NewSession(%#v) = _, %v", sesh, err)
+				}
+			}
+
+			current, err := srv.NewSession(tc.current)
+			if err != nil {
+				t.Errorf("tmux.NewSession(%#v) = _, %v", tc.current, err)
+			}
+
+			got, err := createMenu(current, tc.vcs)
+			if err != nil {
+				t.Errorf("createMenu() = _, %v", err)
+			}
+
+			if diff := cmp.Diff(tc.want, got, cmpopts.IgnoreFields(tmux.MenuEntry{}, "Command")); diff != "" {
+				t.Errorf("createMenu() diff (-want +got)\n%s", diff)
+			}
+		})
+	}
+}

cmd/new.go
@@ -31,7 +31,7 @@ func newWorkUnit(workUnitName string, ctor workUnitCtor) error {
 		return err
 	}
 	srv, _ := tmux.CurrentServerOrDefault()
-	state, err := state.New(srv)
+	state, err := state.New(srv, api.Registered)
 	if err != nil {
 		return err
 	}

cmd/rename.go
@@ -31,7 +31,7 @@ func rename(newName string) error {
 	if err != nil {
 		return err
 	}
-	state, err := state.New(sesh.Server())
+	state, err := state.New(sesh.Server(), api.Registered)
 	if err != nil {
 		return err
 	}

tmux/state/state.go
@@ -17,18 +17,17 @@ import (
 type State struct {
 	srv tmux.Server
 	// tmux sessions in srv with their associated repositories.
-	sessions map[WorkUnitName]tmux.Session
+	sessionsByName map[WorkUnitName]tmux.Session
+	sessionsByID   map[string]workUnit
 	// An index of unqualified repo names that exist in sessions.
 	unqualifiedRepos map[string]int
 	// Representative examples of each api.Repository in sessions.
 	repos map[RepoName]api.Repository
+
+	unknownSessions map[string]tmux.Session
 }
 
-func New(srv tmux.Server) (*State, error) {
-	return newState(srv, api.Registered)
-}
-
-func newState(srv tmux.Server, vcs api.VersionControlSystems) (*State, error) {
+func New(srv tmux.Server, vcs api.VersionControlSystems) (*State, error) {
 	sessions, err := srv.ListSessions()
 	if err != nil {
 		return nil, err
@@ -36,9 +35,11 @@ func newState(srv tmux.Server, vcs api.VersionControlSystems) (*State, error) {
 
 	st := &State{
 		srv:              srv,
-		sessions:         make(map[WorkUnitName]tmux.Session),
+		sessionsByName:   make(map[WorkUnitName]tmux.Session),
+		sessionsByID:     make(map[string]workUnit),
 		unqualifiedRepos: make(map[string]int),
 		repos:            make(map[RepoName]api.Repository),
+		unknownSessions:  make(map[string]tmux.Session),
 	}
 	// An index from directory to api.Repository.
 	// This tool makes tmux sessions with the repository's root dir, so there's a
@@ -66,28 +67,51 @@ func newState(srv tmux.Server, vcs api.VersionControlSystems) (*State, error) {
 			reposByDir[path] = repo
 		}
 		if repo == nil {
+			st.unknownSessions[name] = sesh
 			logger.Info("Not a repository.")
 			continue
 		}
 
 		parsed := ParseSessionName(repo, name)
-		st.sessions[parsed] = sesh
+		st.sessionsByName[parsed] = sesh
+		st.sessionsByID[sesh.ID()] = workUnit{repo, parsed.WorkUnit}
 		st.unqualifiedRepos[parsed.Repo]++
 		st.repos[parsed.RepoName] = repo
-		logger.Info("Found repository in tmux session.", "name", parsed)
+		logger.Info("Found work unit in tmux session.", "name", parsed)
 	}
 	return st, nil
 }
 
-func (st *State) sessionName(n WorkUnitName) string {
+// SessionName returns the string that this State would use for the tmux
+// session name if a work unit with the given name were created right now.
+func (st *State) SessionName(n WorkUnitName) string {
 	if len(st.unqualifiedRepos) > 1 || (len(st.unqualifiedRepos) == 1 && st.unqualifiedRepos[n.Repo] == 0) {
 		return n.RepoString()
 	}
 	return n.WorkUnitString()
 }
 
+// Sessions returns all tmux sessions keyed by their work unit.
 func (st *State) Sessions() map[WorkUnitName]tmux.Session {
-	return maps.Clone(st.sessions)
+	return maps.Clone(st.sessionsByName)
+}
+
+// UnknownSessions returns all tmux sessions that didn't appear to have a work
+// unit.
+func (st *State) UnknownSessions() map[string]tmux.Session {
+	return maps.Clone(st.unknownSessions)
+}
+
+// WorkUnit returns work unit metadata for the given session.
+func (st *State) WorkUnit(sesh tmux.Session) (api.Repository, string, error) {
+	if !tmux.SameServer(sesh.Server(), st.srv) {
+		return nil, "", fmt.Errorf("sesh is from a different server")
+	}
+	n, ok := st.sessionsByID[sesh.ID()]
+	if !ok {
+		return nil, "", fmt.Errorf("sesh does not have an associated work unit")
+	}
+	return n.repo, n.workUnitName, nil
 }
 
 // Repositories returns a representative example for each known RepoName.
@@ -98,7 +122,7 @@ func (st *State) Repositories() map[RepoName]api.Repository {
 // Session determines if a tmux session for the given work unit exists.
 func (st *State) Session(repo api.Repository, workUnitName string) tmux.Session {
 	n := NewWorkUnitName(repo, workUnitName)
-	ret := st.sessions[n]
+	ret := st.sessionsByName[n]
 	if ret != nil {
 		slog.Info("Found existing tmux session for work unit.", "id", ret.ID(), "name", n)
 	}
@@ -109,8 +133,8 @@ func (st *State) Session(repo api.Repository, workUnitName string) tmux.Session 
 // Returns an error if the session already exists.
 func (st *State) NewSession(repo api.Repository, workUnitName string) (tmux.Session, error) {
 	name := NewWorkUnitName(repo, workUnitName)
-	n := st.sessionName(name)
-	if _, ok := st.sessions[name]; ok {
+	n := st.SessionName(name)
+	if _, ok := st.sessionsByName[name]; ok {
 		return nil, fmt.Errorf("tmux session %q already exists", n)
 	}
 
@@ -120,7 +144,8 @@ func (st *State) NewSession(repo api.Repository, workUnitName string) (tmux.Sess
 		return nil, fmt.Errorf("failed to create tmux session %q: %w", n, err)
 	}
 
-	st.sessions[name] = sesh
+	st.sessionsByName[name] = sesh
+	st.sessionsByID[sesh.ID()] = workUnit{repo, name.WorkUnit}
 	st.unqualifiedRepos[name.Repo]++
 	st.repos[name.RepoName] = repo
 	if err := st.updateSessionNames(); err != nil {
@@ -135,21 +160,22 @@ func (st *State) NewSession(repo api.Repository, workUnitName string) (tmux.Sess
 // already a "new" tmux session.
 func (st *State) RenameSession(repo api.Repository, old, new string) error {
 	oldName := ParseSessionName(repo, old)
-	sesh, ok := st.sessions[oldName]
+	sesh, ok := st.sessionsByName[oldName]
 	if !ok {
-		return fmt.Errorf("tmux session %q does not exist", st.sessionName(oldName))
+		return fmt.Errorf("tmux session %q does not exist", st.SessionName(oldName))
 	}
 	newName := NewWorkUnitName(repo, new)
-	if _, ok := st.sessions[newName]; ok {
-		return fmt.Errorf("tmux session %q already exists", st.sessionName(newName))
+	if _, ok := st.sessionsByName[newName]; ok {
+		return fmt.Errorf("tmux session %q already exists", st.SessionName(newName))
 	}
 
-	if err := sesh.Rename(st.sessionName(newName)); err != nil {
+	if err := sesh.Rename(st.SessionName(newName)); err != nil {
 		return err
 	}
 
-	delete(st.sessions, oldName)
-	st.sessions[newName] = sesh
+	delete(st.sessionsByName, oldName)
+	st.sessionsByName[newName] = sesh
+	st.sessionsByID[sesh.ID()] = workUnit{repo, newName.WorkUnit}
 
 	if err := st.updateSessionNames(); err != nil {
 		slog.Warn("Failed to update tmux session names.", "error", err)
@@ -195,7 +221,8 @@ func (st *State) PruneSessions() error {
 		if err := sesh.Kill(); err != nil {
 			return err
 		}
-		delete(st.sessions, n)
+		delete(st.sessionsByName, n)
+		delete(st.sessionsByID, sesh.ID())
 		st.unqualifiedRepos[n.Repo]--
 		if st.unqualifiedRepos[n.Repo] == 0 {
 			delete(st.unqualifiedRepos, n.Repo)
@@ -211,13 +238,13 @@ func (st *State) PruneSessions() error {
 
 func (st *State) updateSessionNames() error {
 	var errs []error
-	for k, sesh := range st.sessions {
+	for k, sesh := range st.sessionsByName {
 		name, err := sesh.Property(tmux.SessionName)
 		if err != nil {
 			errs = append(errs, err)
 			continue
 		}
-		if want := st.sessionName(k); name != want {
+		if want := st.SessionName(k); name != want {
 			if err := sesh.Rename(want); err != nil {
 				errs = append(errs, err)
 				continue
@@ -227,8 +254,8 @@ func (st *State) updateSessionNames() error {
 	return errors.Join(errs...)
 }
 
-// MaybeFindRepository attempts to find an api.Repository that claims the given
-// work unit exists.
+// MaybeFindRepository attempts to find an api.Repository that's currently
+// active in tmux and claims that the given work unit exists.
 // Returns an error if multiple api.Repositories claim that the given work unit
 // exists.
 // Returns nil, nil if no such api.Repository exists.
@@ -300,4 +327,13 @@ func (n WorkUnitName) WorkUnitString() string {
 
 func (n WorkUnitName) LogValue() slog.Value {
 	return slog.GroupValue(slog.String("vcs", n.VCS), slog.String("repo", n.Repo), slog.String("work_unit", n.WorkUnit))
+}
+
+type workUnit struct {
+	repo         api.Repository
+	workUnitName string
+}
+
+func (wu workUnit) name() WorkUnitName {
+	return NewWorkUnitName(wu.repo, wu.workUnitName)
 }

tmux/state/state_test.go
@@ -67,6 +67,7 @@ func TestNew(t *testing.T) {
 				},
 				UnqualifiedRepos: []string{"repo"},
 				Repos:            []RepoName{{Repo: "repo"}},
+				UnknownSessions:  []string{"bar"},
 			},
 		},
 		{
@@ -109,11 +110,11 @@ func TestNew(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			st, err := newState(tc.tmux, tc.vcs)
+			st, err := New(tc.tmux, tc.vcs)
 			if err != nil {
-				t.Errorf("newState() = _, %v", err)
+				t.Errorf("New() = _, %v", err)
 			}
-			if diff := cmp.Diff(tc.want, simplifyState(st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
+			if diff := cmp.Diff(tc.want, simplifyState(t, st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
 				t.Errorf("State diff (-want +got)\n%s", diff)
 			}
 		})
@@ -269,9 +270,9 @@ func TestNewSession(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			st, err := newState(tc.tmux, tc.vcs)
+			st, err := New(tc.tmux, tc.vcs)
 			if err != nil {
-				t.Fatalf("newState() = _, %v", err)
+				t.Fatalf("New() = _, %v", err)
 			}
 			repo, err := tc.vcs.MaybeFindRepository(tc.repoDir)
 			if err != nil {
@@ -285,7 +286,7 @@ func TestNewSession(t *testing.T) {
 				t.Errorf("NewSession(%q, %q) = %v, wantErr %t", tc.repoDir, tc.workUnitName, err, tc.wantErr)
 			}
 
-			if diff := cmp.Diff(tc.want, simplifyState(st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
+			if diff := cmp.Diff(tc.want, simplifyState(t, st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
 				t.Errorf("State diff (-want +got)\n%s", diff)
 			}
 			if diff := cmp.Diff(tc.wantTmux, simplifyTmuxState(tc.tmux), compareSimplifiedTmuxState); diff != "" {
@@ -492,9 +493,9 @@ func TestRename(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			st, err := newState(tc.tmux, tc.vcs)
+			st, err := New(tc.tmux, tc.vcs)
 			if err != nil {
-				t.Fatalf("newState() = _, %v", err)
+				t.Fatalf("New() = _, %v", err)
 			}
 			repo, err := tc.vcs.MaybeFindRepository(tc.repoDir)
 			if err != nil {
@@ -508,7 +509,7 @@ func TestRename(t *testing.T) {
 				t.Errorf("RenameSession(%q, %q, %q) = %v, wantErr = %t", tc.repoDir, tc.old, tc.new, err, tc.wantErr)
 			}
 
-			if diff := cmp.Diff(tc.want, simplifyState(st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
+			if diff := cmp.Diff(tc.want, simplifyState(t, st), compareSimplifiedStates, cmpopts.IgnoreFields(RepoName{}, "VCS")); diff != "" {
 				t.Errorf("State diff (-want +got)\n%s", diff)
 			}
 			if diff := cmp.Diff(tc.wantTmux, simplifyTmuxState(tc.tmux), compareSimplifiedTmuxState); diff != "" {
@@ -522,6 +523,7 @@ type simplifiedState struct {
 	WorkUnits        []WorkUnitName
 	UnqualifiedRepos []string
 	Repos            []RepoName
+	UnknownSessions  []string
 }
 
 var repoCmp = morecmp.Comparing(func(n RepoName) string { return n.VCS }).
@@ -535,16 +537,30 @@ var compareSimplifiedStates = cmp.Options{
 	cmpopts.SortSlices(repoCmp.LessFunc()),
 }
 
-func simplifyState(st *State) simplifiedState {
+func simplifyState(t *testing.T, st *State) simplifiedState {
+	t.Helper()
 	var ret simplifiedState
-	for n := range st.sessions {
+	for n, sesh := range st.sessionsByName {
 		ret.WorkUnits = append(ret.WorkUnits, n)
+
+		if wu, ok := st.sessionsByID[sesh.ID()]; !ok || wu.name() != n {
+			t.Errorf("sessionsByID[%q] = %q, %t, expected %q", sesh.ID(), wu.name(), ok, n)
+		}
+	}
+	for id, wu := range st.sessionsByID {
+		n := wu.name()
+		if _, ok := st.sessionsByName[n]; !ok {
+			t.Errorf("sessionsByName[%q] is missing, expected sesh %q", n, id)
+		}
 	}
 	for n := range st.unqualifiedRepos {
 		ret.UnqualifiedRepos = append(ret.UnqualifiedRepos, n)
 	}
 	for n := range st.repos {
 		ret.Repos = append(ret.Repos, n)
+	}
+	for n := range st.unknownSessions {
+		ret.UnknownSessions = append(ret.UnknownSessions, n)
 	}
 	return ret
 }

commit f5b1f1ff27492baa6bf4cc91e9efb7025b03a74c

fix(update): Fix update completion when not attached to tmux.
Broken by #19 (https://github.com/JeffFaer/tmux-vcs-sync/commit/0f4a2222c9f3a483d891faed2d3b5d4c53349337).

cmd/update.go
@@ -42,7 +42,7 @@ var updateCommand = &cobra.Command{
 }
 
 func suggestWorkUnitNames(toComplete string) []string {
-	var repos map[state.RepoName]api.Repository
+	repos := make(map[state.RepoName]api.Repository)
 	if srv := tmux.MaybeCurrentServer(); srv != nil {
 		st, err := state.New(srv, api.Registered)
 		if err != nil {