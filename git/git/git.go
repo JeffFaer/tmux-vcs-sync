21047a438c8c6a023bf644a53d00c6d2745cc80a
Jeffrey Faer <jeffrey.faer@gmail.com>
2026-09-21 16:21:45 -0600

    fix(git): Don't perform unnecessary git commands while sorting if there's only a single work unit. (#40)

--- git/git/git.go
+++ git/git/git.go
@@ -158,6 +158,12 @@ func (repo *gitRepo) Sort(ctx context.Context, workUnits []string) error {
 	if len(workUnits) == 0 {
 		return nil
 	}
+	if len(workUnits) == 1 {
+		if repo.branchExists(ctx, workUnits[0]) {
+			return nil
+		}
+		return fmt.Errorf("work unit does not exist: %s", workUnits[0])
+	}
 
 	branchesByHash, err := repo.keyBranchByHash(ctx, workUnits)
 	if err != nil {
